54615d9d96fa7d7caa0731cae53e204eb9d8f52b
Termi <escarpaprieto@gmail.com>
2021-11-25 20:07:35 +0100

    altern visible options and trackbar value

--- passGen/passGen/MainForm.cs
+++ passGen/passGen/MainForm.cs
@@ -12,14 +12,64 @@ namespace passGen
 {
     public partial class MainForm : Form
     {
+        private int heightChange = 60;
         public MainForm()
         {
             InitializeComponent();
+            closeOptions();
+            changeLengthValue();
         }
 
-        private void options_btn_Click(object sender, EventArgs e)
+        #region events
+
+        private void options_btn_Click(object sender, EventArgs e) => optionsClick();
+
+        private void trackBar_Scroll(object sender, EventArgs e) => changeLengthValue();
+
+        #endregion
+
+        #region private methods
+
+        #region options
+        private void optionsClick()
         {
-
+            switch (options_panel.Visible)
+            {
+                case true:
+                    closeOptions();
+                    Height -= heightChange;
+                    break;
+                case false:
+                    openOptions();
+                    Height += heightChange;
+                    break;
+            }
         }
+
+        private void closeOptions() => options_panel.Visible = false;
+
+        private void openOptions() => options_panel.Visible = true;
+        #endregion
+
+        #region trackBar
+
+        private void changeLengthValue()
+        {
+            passLength_lbl.Text = $"Password length: {trackBar.Value.ToString()}";
+        }
+
+        #endregion
+
+        #region genButton
+
+        #endregion
+
+        #region copyButton
+
+        #endregion
+
+        #endregion
+
+
     }
 }
