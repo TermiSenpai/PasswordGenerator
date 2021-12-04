78a3d8cefbdc76a69b46cfc976a313654c75abb1
Termi <escarpaprieto@gmail.com>
2021-12-04 17:44:59 +0100

    squirrel updater deleted

--- passGen/passGen/Program.cs
+++ passGen/passGen/Program.cs
@@ -3,21 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Squirrel;
 
 namespace passGen
 {
     static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-        [STAThread]
-        static async Task Main()
+        static void Main(string[] args)
         {
-            /*
-            using (var mgr = new UpdateManager("https://github.com/TermiSenpai/PasswordGenerator/releases/latest"))
-                await mgr.UpdateApp(); */
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
